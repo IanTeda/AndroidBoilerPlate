3cd7affdf4ef18dfd0aa7ef17af2e7a41af7ba40
Ian Teda <ian@teda.id.au>
2013-04-09 10:01:28 +1000

    Added base menu layout

--- src/au/id/teda/androidboilerplate/activity/BaseActivity.java
+++ src/au/id/teda/androidboilerplate/activity/BaseActivity.java
@@ -57,7 +57,7 @@ public class BaseActivity extends SherlockFragmentActivity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getSupportMenuInflater().inflate(R.menu.main, menu);
+		getSupportMenuInflater().inflate(R.menu.base_menu, menu);
 		
 		return true;
 	}
