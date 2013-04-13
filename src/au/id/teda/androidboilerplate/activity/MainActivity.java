5dee7afcb79c92a813600289f2998e29c76d125a
Teeds (Macbook Pro) <ian@teda.id.au>
2013-04-13 19:42:31 +1000

    Rebase commit

--- src/au/id/teda/androidboilerplate/activity/MainActivity.java
+++ src/au/id/teda/androidboilerplate/activity/MainActivity.java
@@ -9,6 +9,8 @@ public class MainActivity extends BaseActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//Rebase commit
 	}
 
 
